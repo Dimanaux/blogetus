12edd871a3252984f9da1484d02e2030445d037c
Vladimir Bazhanov <vladimir.bazhanov@flatstack.com>
2019-02-17 14:36:07 +0300

    Fresh puma config

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # BLOGETUS
 
 [![Build Status](https://semaphoreci.com/api/v1/vladimirbazhanov/blogetus/branches/master/badge.svg)](https://semaphoreci.com/vladimirbazhanov/blogetus)
+
--- config/puma.rb
+++ config/puma.rb
@@ -1,15 +1,13 @@
-workers Integer(ENV['WEB_CONCURRENCY'] || 2)
-threads_count = Integer(ENV['RAILS_MAX_THREADS'] || 5)
+workers ENV.fetch("WEB_CONCURRENCY", 2).to_i
+threads_count = ENV.fetch("MAX_THREADS", 5).to_i
 threads threads_count, threads_count
 
 preload_app!
 
 rackup      DefaultRackup
-port        ENV['PORT']     || 5000
-environment ENV['RACK_ENV'] || 'development'
+port        ENV.fetch("PORT", 5000)
+environment ENV.fetch("RACK_ENV", "development")
 
 on_worker_boot do
-  # Worker specific setup for Rails 4.1+
-  # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
   ActiveRecord::Base.establish_connection
 end
