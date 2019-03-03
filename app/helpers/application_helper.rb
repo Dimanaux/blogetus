2eb4dc1f3ef613f5185d493abe1edcbcfbe79af8
Dmitry Barskov <barskoidim@gmail.com>
2019-03-04 02:01:31 +0300

    add helper for md

---
+++ app/helpers/application_helper.rb
@@ -0,0 +1,12 @@
+module ApplicationHelper
+  def markdown(text)
+    # options = {
+    #   :fenced_code_blocks => true,
+    #   :no_intra_emphasis => true,
+    #   :autolink => true,
+    #   :lax_html_blocks => true,
+    # }
+    markdown_to_html = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    markdown_to_html.render(text).html_safe
+  end
+end
