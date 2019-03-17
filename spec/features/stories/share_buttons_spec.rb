9824aa2ee5a9a1529114ec506948feea8b81abaf
Dmitry Barskov <barskoidim@gmail.com>
2019-03-17 13:30:04 +0300

    add test for share buttons presense

---
+++ spec/features/stories/share_buttons_spec.rb
@@ -0,0 +1,25 @@
+require "rails_helper"
+
+describe "Share buttons" do
+  let!(:story) { create :story }
+
+  it "facebook btn present" do
+    visit story_path(story)
+
+    expect(page).to have_content("Facebook")
+  end
+
+  it "Twitter btn present" do
+    visit story_path(story)
+
+    puts page 
+
+    expect(page).to have_content("Twitter")
+  end
+
+  it "Vkontakte btn present" do
+    visit story_path(story)
+
+    expect(page).to have_content("VK")
+  end
+end
