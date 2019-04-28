cf2df81de9d92f22e11c26b33cde16afd8f715b9
Dmitry Barskov <barskoidim@gmail.com>
2019-04-28 17:45:47 +0300

    add unlike feature to controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,6 @@
 class LikesController < ApplicationController
   before_action :find_story
+  before_action :find_like, only: [:destroy]
 
   def create
     if already_liked?
@@ -10,12 +11,26 @@ class LikesController < ApplicationController
     redirect_to story_path(@story)
   end
 
+  def destroy
+    if !(already_liked?)
+      flash[:notice] = "Cannot unlike"
+    else
+      @like.destroy
+    end
+    redirect_to story_path(@story)
+  end
+
   private
+
   def find_story
     @story = Story.find(params[:story_id])
   end
 
   def already_liked?
     Like.where(user_id: current_user.id, story_id: params[:story_id]).exists?
+  end
+
+  def find_like
+    @like = @story.likes.find(params[:id])
   end
 end
